2573c77c1da855497be09ebb7c3cc3e6a3d4378d
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2024-06-22 15:20:11 +0000

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Query
+
+app = FastAPI(
+    title="Atlas",
+    summary="Hallucination-detecting API.",
+    description="Search the web for queries and compare results with LLM to detect and mitigate hallucinations.",
+    version="0.0.1",
+)
+
+
+@app.get("/api")
+async def search(
+    query: str = Query(
+        ...,
+        description="Query to search the web",
+    ),
+):
+    """Search the web.
+
+    Parameters
+    ----------
+    query : str
+        _description_, by default Query( ..., description="Query to search the web", )
+
+    Returns
+    -------
+    json
+        Results from searching the web.
+    """
+    return {"query": query}
