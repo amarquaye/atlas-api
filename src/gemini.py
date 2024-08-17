2c3051b27ff0d10c24045699298b51c91296df08
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2024-08-17 17:11:18 +0000

    Merge from dev branch (#8)
    * Added /verify path
    
    * Updated src/gemini.py
    
    Added hallucination mechanism
    
    * Updated src
    
    Made some changes to src/gemini.py so it detects hallucinations

--- src/gemini.py
+++ src/gemini.py
@@ -30,15 +30,40 @@ def generate_query(query: str) -> str:
         generation_config=generation_config,
         # safety_settings = Adjust safety settings
         # See https://ai.google.dev/gemini-api/docs/safety-settings
-        system_instruction="""Your name is Biba. You are a prompt engineering assistant. 
+        system_instruction="""You are a prompt engineering assistant. 
         Your duty is to take in a query and generate a suitable google search query that will provides relevant results from the web. 
         The user will type the query but they might not be specific or tailored well to get the appropriate response from an LLM. 
         So you will scan through the text the user inputs and refine it to generate a well-suited query for a google search that will return the results the user is looking for. 
-        But remember this, the entire response you are to return should be less than 32 words long""",
+        But remember this, the entire response you are to return should be less than 32 words long and don't include 'Google Search' or special characters like '\n in your response.""",
     )
-
     chat_session = model.start_chat(history=[])
-
     response = chat_session.send_message(query)
+
+    return response.text
+
+
+def cmp(llm_response: str, search_result: str) -> str:
+    model = genai.GenerativeModel(
+        model_name="gemini-1.5-flash",
+        generation_config=generation_config,
+        # safety_settings = Adjust safety settings
+        # See https://ai.google.dev/gemini-api/docs/safety-settings
+        system_instruction="""You are a prompt engineering assistant. 
+        Your duty is to take in two responses; first one is the response from an LLM(llm_response) and the second is the results from a web search(search_results) which is rendered in markdown for you to easily extract relevant information or detail, both provided as parameters.
+        Your goal is to compare the two responses and determine if the text in the parameter 'llm_response' is or not a deviation from 'search_results'.
+        You are tasked to look through both responses and determine the differences and find out whether the response from the LLM is aligned with the results from the web search to detect any form of hallucination.
+        Look at both results and if they are similar do not return Hallucination detected.
+        Sometimes you claim some responses contain hallucinations even though the response from the LLM is almost inline with the search results.
+        Only raise the alert of a hallucination detection when the LLM's response completely deviates from the search results and see through both responses yourself and determine if the LLM's response is truly hallucinating.
+        If there is any hallucination detected, return the text response: Hallucination detected.
+        Then a json object with key of 'LLM response' with the value being the part of the LLM's response where the hallucination occurred. 
+        And another key of 'Search result' with the value being the refined section of the search result which shows the correct answer as compared to the llm's response. 
+        Or return No hallucination if the llm_response and search_result are aligned and there is no hallucination after comparing both responses.        
+        """,
+    )
+    chat_session = model.start_chat(history=[])
+    response = chat_session.send_message(
+        f"The LLM's response is {llm_response} and the search results is {search_result}"
+    )
 
     return response.text
